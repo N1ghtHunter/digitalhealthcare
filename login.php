9fd9ed558fc864a3ccd6c0127d8a5bbe9ec2f49c
Mazin Islam <93228608+N1ghtHunter@users.noreply.github.com>
2023-04-30 16:23:33 +0300

    Conflict workaround

--- login.php
+++ login.php
@@ -1,11 +1,4 @@
 <?php
-
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-?>
-
-<?php
 session_start();
 
 $email = '';
@@ -55,72 +48,27 @@ if (isset($_SESSION['email'])) {
                 <div class="contact">
 
                     <form action="api/patient/create.php" method="POST">
-                        <h3>SIGN UP</h3>
-                        <input type="text" name="first_name" placeholder="First Name *"
-                            value="<?php echo $firstname; ?>" required>
-                        <input type="text" name="last_name" placeholder="Last Name *" value="<?php echo $lastname; ?>"
-                            required>
-                        <?php if (isset($_SESSION['name_error'])) { ?>
-                        <p class="error"><?php echo $_SESSION['name_error']; ?></p>
-                        <?php
-                            unset($_SESSION['name_error']);
-                        } ?>
+                        <h3>SIGN IN</h3>
                         <input type="email" name="email" placeholder="Email *" value="<?php echo $email; ?>" required>
                         <?php if (isset($_SESSION['email_error'])) { ?>
                         <p class="error"><?php echo $_SESSION['email_error']; ?></p>
                         <?php
                             unset($_SESSION['email_error']);
                         } ?>
-                        <input type="text" name="phone_number" placeholder="Phone *"
-                            value="<?php echo $phone_number; ?>" required>
-                        <p>Gender: *</p>
-                        <div class="wrapper">
-                            <input type="radio" name="gender" value="female" value="male" id="option-1" checked>
-                            <input type="radio" name="gender" id="option-2">
-                            <label for="option-1" class="option option-1">
-                                <div class="dot"></div>
-                                <span>Male</span>
-                            </label>
-                            <label for="option-2" class="option option-2">
-                                <div class="dot"></div>
-                                <span>Female</span>
-                            </label>
-                        </div>
                         <!-- date -->
-                        <label for="birthday" style="margin-bottom: 0; margin-top:8px;">Date of birth *</label>
-                        <input type="date" id="birthday" name="date_of_birth" value="<?php echo $date_of_birth; ?>"
-                            required>
                         <input type="password" name="password" placeholder="Password" required>
                         <?php if (isset($_SESSION['password_error'])) { ?>
                         <p class="error"><?php echo $_SESSION['password_error']; ?></p>
                         <?php
                             unset($_SESSION['password_error']);
                         } ?>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <label for="blood_type" style="margin-bottom: 0; margin-top:0px;">Blood
-                                Type:</label>
-                            <select name="blood_type" id="blood_type" class="blood_type px-2"
-                                value="<?php echo $blood_type; ?>" required>
-                                <option value="" selected disabled hidden>Select Blood Type</option>
-                                <option value="A+">A+</option>
-                                <option value="A-">A-</option>
-                                <option value="B+">B+</option>
-                                <option value="B-">B-</option>
-                                <option value="AB+">AB+</option>
-                                <option value="AB-">AB-</option>
-                                <option value="O+">O+</option>
-                                <option value="O-">O-</option>
-                            </select>
-                        </div>
-                        <input type="text" name="insurance_info" placeholder="Insurance Info"
-                            value="<?php echo $insurance_info; ?>">
-                        <input type="text" name="age" placeholder="Age *" value="<?php echo $age; ?>" required>
                         <?php if (isset($_SESSION['error'])) { ?>
                         <p class="error"><?php echo $_SESSION['error']; ?></p>
                         <?php
                             unset($_SESSION['error']);
                         } ?>
                         <input id="submit" type="submit" value="Sign Up">
+                        <p>Already have an account? <a href="login.php">Login</a></p>
                     </form>
                 </div>
             </div>
